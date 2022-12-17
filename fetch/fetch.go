52ac781e1129ae6b732ee8cecaa8b87d68b7e7d3
Pol Milian <pol.milian.dev@gmail.com>
2022-12-17 16:33:57 +0100

    moved to package

---
+++ fetch/fetch.go
@@ -0,0 +1,40 @@
+package fetch
+
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	gogpt "github.com/sashabaranov/go-gpt3"
+)
+
+func Fetch() (string, error) {
+	godotenv.Load()
+
+	apiKey := os.Getenv("OPENAI_API_KEY")
+
+	if apiKey == "" {
+		log.Fatalln("Missing API KEY")
+	}
+
+	c := gogpt.NewClient(apiKey)
+	ctx := context.Background()
+
+	req := gogpt.CompletionRequest{
+		Model:     "text-davinci-003",
+		MaxTokens: 256,
+		Prompt:    os.Getenv("PROMPT"),
+	}
+
+	resp, err := c.CreateCompletion(ctx, req)
+
+	if err != nil {
+		return "", err
+	}
+
+	trimmed := strings.TrimSpace(resp.Choices[0].Text)
+
+	return trimmed, err
+}
--- main.go
+++ main.go
@@ -1,38 +1,19 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"os"
-	"strings"
 
-	"github.com/joho/godotenv"
-	gogpt "github.com/sashabaranov/go-gpt3"
+	"github.com/el-pol/lovebot/fetch"
 )
 
 func main() {
-	godotenv.Load()
-
-	apiKey := os.Getenv("OPENAI_API_KEY")
-
-	if apiKey == "" {
-		log.Fatalln("Missing API KEY")
-	}
-
-	c := gogpt.NewClient(apiKey)
-	ctx := context.Background()
-
-	req := gogpt.CompletionRequest{
-		Model:     "text-davinci-003",
-		MaxTokens: 256,
-		Prompt:    os.Getenv("PROMPT"),
-	}
-	resp, err := c.CreateCompletion(ctx, req)
+	result, err := fetch.Fetch()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(strings.TrimSpace(resp.Choices[0].Text))
+	fmt.Println(result)
+
 }
