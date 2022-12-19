c5e4193901c0631b2aac108e313fc25a6a009144
Pol Milian <pol.milian.dev@gmail.com>
2022-12-19 20:25:55 +0100

    sending a tweet

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/el-pol/lovebot
 go 1.19
 
 require (
+	github.com/dghubble/oauth1 v0.7.2
 	github.com/joho/godotenv v1.4.0
 	github.com/sashabaranov/go-gpt3 v0.0.0-20221216095610-1c20931ead68
 )
--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 
+	"os"
+
 	"github.com/el-pol/lovebot/fetch"
+	"github.com/joho/godotenv"
+
+	"github.com/dghubble/oauth1"
 )
 
 func main() {
+	godotenv.Load()
 	result, err := fetch.Fetch()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(result)
+	consumerKey := os.Getenv("CONSUMER_KEY")
+	consumerSecret := os.Getenv("CONSUMER_SECRET")
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	accessSecret := os.Getenv("TOKEN_SECRET")
+
+	if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
+		panic("Missing required environment variable")
+	}
+
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	path := "https://api.twitter.com/2/tweets"
+
+	jsonBody := fmt.Sprintf(`{"text": "%s"}`, result)
+
+	bodyReader := bytes.NewReader([]byte(jsonBody))
+	resp, err := httpClient.Post(path, "application/json", bodyReader)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Printf("Raw Response Body:\n%v\n", string(body))
 
 }
