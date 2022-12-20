64a9c96039f45577b9fb4885973ca57ac5218d86
Pol Milian <pol.milian.dev@gmail.com>
2022-12-20 16:12:28 +0100

    move all to main

--- go.mod
+++ go.mod
@@ -7,5 +7,3 @@ require (
 	github.com/joho/godotenv v1.4.0
 	github.com/sashabaranov/go-gpt3 v0.0.0-20221216095610-1c20931ead68
 )
-
-require github.com/aws/aws-lambda-go v1.36.0 // indirect
--- main.go
+++ main.go
@@ -2,38 +2,61 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	"os"
 
-	"github.com/el-pol/lovebot/fetch"
 	"github.com/joho/godotenv"
+	gogpt "github.com/sashabaranov/go-gpt3"
 
 	"github.com/dghubble/oauth1"
 )
 
 func main() {
 	godotenv.Load()
-	result := fetch.Fetch()
 
-	if result == "" {
-		log.Fatalln("Result is empty")
-	}
-
-	if len(result) >= 280 {
-		log.Fatalln("Result is too long")
-	}
-
+	apiKey := os.Getenv("OPENAI_API_KEY")
 	consumerKey := os.Getenv("CONSUMER_KEY")
 	consumerSecret := os.Getenv("CONSUMER_SECRET")
 	accessToken := os.Getenv("ACCESS_TOKEN")
 	accessSecret := os.Getenv("TOKEN_SECRET")
 
-	if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
+	if apiKey == "" || consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
 		panic("Missing required environment variable")
 	}
 
+	// Fetch from GPT-3
+	c := gogpt.NewClient(apiKey)
+	ctx := context.Background()
+
+	req := gogpt.CompletionRequest{
+		Model:     "text-davinci-003",
+		MaxTokens: 256,
+		Prompt:    os.Getenv("PROMPT"),
+	}
+
+	resp, err := c.CreateCompletion(ctx, req)
+
+	if err != nil {
+		log.Fatalf("Error when creating completion: %v", err)
+	}
+
+	log.Printf("Completion was successful: %+v", resp)
+
+	trimmed := strings.TrimSpace(resp.Choices[0].Text)
+
+	if trimmed == "" {
+		log.Fatalln("Result is empty")
+	}
+
+	if len(trimmed) >= 280 {
+		log.Fatalln("Result is too long")
+	}
+
+	// Post to Twitter
 	config := oauth1.NewConfig(consumerKey, consumerSecret)
 	token := oauth1.NewToken(accessToken, accessSecret)
 
@@ -41,14 +64,16 @@ func main() {
 
 	path := "https://api.twitter.com/2/tweets"
 
-	jsonBody := fmt.Sprintf(`{"text": "%s"}`, result)
+	jsonBody := fmt.Sprintf(`{"text": "%s"}`, trimmed)
 
 	bodyReader := bytes.NewReader([]byte(jsonBody))
-	resp, err := httpClient.Post(path, "application/json", bodyReader)
+
+	response, err := httpClient.Post(path, "application/json", bodyReader)
+
 	if err != nil {
 		log.Fatalf("Error when posting to twitter: %v", err)
 	}
-	defer resp.Body.Close()
-	log.Printf("Response was OK: %v", resp)
 
+	defer response.Body.Close()
+	log.Printf("Response was OK: %v", response)
 }
